dc73bf5fa974fdabe76d8c96131adbbcefa7bdfe
Ksenia-cha <kseniyachapygova@gmail.com>
2022-11-22 10:36:44 +0300

    fix error handling for get requests to db and rtsp

--- internal/rtsp-simple-server/interface.go
+++ internal/rtsp-simple-server/interface.go
@@ -3,7 +3,7 @@ package rtspsimpleserver
 import "github.com/Kseniya-cha/System-for-raising-video-streams/internal/refreshstream"
 
 type RTSPCommon interface {
-	GetRtsp() map[string]interface{}
+	GetRtsp() (map[string]interface{}, error)
 	PostAddRTSP(camDB refreshstream.RefreshStream) error
 	PostRemoveRTSP(camRTSP string) error
 	PostEditRTSP(camDB refreshstream.RefreshStream, conf Conf) error
