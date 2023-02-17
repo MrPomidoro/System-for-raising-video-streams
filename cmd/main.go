6dfe5b53eee5770266135cfa907c5afc7a103ed3
Ksenia-cha <kseniyachapygova@gmail.com>
2023-02-17 11:17:18 +0300

    add to main logging and custom Error

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/config"
+	ce "github.com/Kseniya-cha/System-for-raising-video-streams/pkg/customError"
+	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/logger"
 	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/service"
 )
 
@@ -12,10 +14,16 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	// Чтение конфигурационного файла
-	cfg, _ := config.GetConfig()
-
+	cfg, err := config.GetConfig()
+	if err != nil {
+		return
+	}
+	log := logger.NewLogger(cfg)
 	// Инициализация прототипа приложения
-	app := service.NewApp(ctx, cfg)
+	app, err := service.NewApp(ctx, cfg)
+	if err != nil {
+		log.Error(ce.NewError(ce.FatalLevel, "50.0.0", "error at main package level").Error())
+	}
 
 	// Запуск алгоритма в отдельной горутине
 	go app.Run(ctx)
