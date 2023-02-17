73fd8501e4ee9a38d39933fe686100e1eff46dc9
Ksenia-cha <kseniyachapygova@gmail.com>
2023-02-17 15:27:56 +0300

    edit type custom error to linked list

--- pkg/customError/customError.go
+++ pkg/customError/customError.go
@@ -1,5 +1,10 @@
 package customError
 
+import (
+	"fmt"
+	"strings"
+)
+
 const (
 	WarnLevel = iota
 	ErrorLevel
@@ -7,19 +12,38 @@ const (
 )
 
 type IError interface {
+	Error() string
+
 	SetLevel()
+	SetError(err error) *Error
+	SetCode(code string)
+	SetDesc(desc string)
 }
 
 type Error struct {
 	level int    // уровень ошибки (warn, error, fatal etc)
 	code  string // код ошибки
-	err   error
 	desc  string // напр, эта ошибка вызвана отказоустойчивостью бд
+	err   error  // текст ошибки
+	deep  *Error // ссылка на структуру вложенной ошибки
 }
 
 // TODO: здесь будет формироваться красивенький вывод
-func (e *Error) Error() string {
-	return e.err.Error()
+func (e Error) Error() string {
+	output := strings.Builder{}
+
+	for e.deep != nil {
+
+		output.WriteString(fmt.Sprintf("level: %d, code: %s, description: %s, error: %v;\n",
+			e.level, e.code, e.desc, e.err))
+
+		e = *e.deep
+	}
+
+	output.WriteString(fmt.Sprintf("level: %d, code: %s, description: %s, error: %v",
+		e.level, e.code, e.desc, e.err))
+
+	return output.String()
 }
 
 func NewError(level int, code, desc string) *Error {
@@ -30,13 +54,20 @@ func NewError(level int, code, desc string) *Error {
 	}
 }
 
-func (e *Error) SetLevel(level int) {
-	e.level = level
+// NextError создаёт новую ошибку, наследуя переданную deep
+// level, code, desc, err задаются отдельно функциями SetXXX
+func (e *Error) NextError(deep *Error) *Error {
+	e.deep = deep
+	return e
 }
 
 func (e *Error) SetError(err error) *Error {
 	e.err = err
 	return e
+}
+
+func (e *Error) SetLevel(level int) {
+	e.level = level
 }
 
 func (e *Error) SetCode(code string) {
