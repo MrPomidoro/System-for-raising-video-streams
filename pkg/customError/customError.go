46d4c848b7771e19d6511594dc27d728adcaa444
Ksenia-cha <kseniyachapygova@gmail.com>
2023-02-17 10:21:36 +0300

    create custom error

---
+++ pkg/customError/customError.go
@@ -0,0 +1,42 @@
+package customError
+
+const (
+	WarnLevel = iota
+	ErrorLevel
+	FatalLevel
+)
+
+type IError interface {
+	SetLevel()
+}
+
+type Error struct {
+	level int    // уровень ошибки (warn, error, fatal etc)
+	code  string // код ошибки
+	err   error
+	desc  string // напр, эта ошибка вызвана отказоустойчивостью бд
+}
+
+// TODO: здесь будет формироваться красивенький вывод
+func (e *Error) Error() string {
+	return e.err.Error()
+}
+
+func NewError(level int, code, desc string) *Error {
+	return &Error{
+		level: level,
+		code:  code,
+		desc:  desc,
+	}
+}
+
+func (e *Error) SetLevel(level int) {}
+
+func (e *Error) SetError(err error) *Error {
+	e.err = err
+	return e
+}
+
+func (e *Error) Marshal() {}
+
+func (e *Error) UnMarshal() {}
