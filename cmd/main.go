7595b067a43a1923ba7b70034b4961305227ee1a
Ksenia-cha <kseniyachapygova@gmail.com>
2023-02-20 07:53:36 +0300

    edit error processing in main

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/config"
 	ce "github.com/Kseniya-cha/System-for-raising-video-streams/pkg/customError"
-	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/logger"
 	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/service"
 )
 
+// /*
 func main() {
 	// Инициализация контекста
 	ctx, cancel := context.WithCancel(context.Background())
+	cerr := ce.NewError(ce.FatalLevel, "50.0.0", "error at main package level")
 
 	// Чтение конфигурационного файла
 	cfg, err := config.GetConfig()
 	if err != nil {
+		cerr.NextError(err)
+		fmt.Println(cerr.SetError(err).Error())
 		return
 	}
-	log := logger.NewLogger(cfg)
+
+	// log := logger.NewLogger(cfg)
+
 	// Инициализация прототипа приложения
 	app, err := service.NewApp(ctx, cfg)
 	if err != nil {
-		log.Error(ce.NewError(ce.FatalLevel, "50.0.0", "error at main package level").Error())
+		cerr.NextError(err)
+		fmt.Println(cerr.Error())
+		// fmt.Println(cerr.SetError().Error())
+		return
 	}
 
 	// Запуск алгоритма в отдельной горутине
@@ -32,3 +41,5 @@ func main() {
 	// app.GracefulShutdown(app.SigChan)
 	app.GracefulShutdown(ctx, cancel)
 }
+
+// */
