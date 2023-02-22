4690815cdd327e51075a73e309768d8a0a5d3f34
Ksenia-cha <kseniyachapygova@gmail.com>
2023-02-22 09:17:03 +0300

    add comments, chan and context

--- internal/rtsp-simple-server/interface.go
+++ internal/rtsp-simple-server/interface.go
@@ -1,14 +1,20 @@
 package rtspsimpleserver
 
 import (
+	"context"
+
 	"github.com/Kseniya-cha/System-for-raising-video-streams/internal/refreshstream"
 	ce "github.com/Kseniya-cha/System-for-raising-video-streams/pkg/customError"
 )
 
 type RTSPCommon interface {
-	GetRtsp() ([]SConf, ce.IError)
+	// GetRtsp отправляет GET запрос на получение данных
+	GetRtsp(ctx context.Context, dataRTSPchan chan SConf) ([]SConf, ce.IError)
+	// PostAddRTSP отправляет POST запрос на добавление потока
 	PostAddRTSP(camDB refreshstream.RefreshStream) ce.IError
+	// PostRemoveRTSP отправляет POST запрос на удаление потока
 	PostRemoveRTSP(camRTSP string) ce.IError
+	// PostEditRTSP отправляет POST запрос на изменение потока
 	PostEditRTSP(camDB refreshstream.RefreshStream, conf SConf) ce.IError
 }
 
