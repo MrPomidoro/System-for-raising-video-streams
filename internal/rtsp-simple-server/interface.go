3ac95dc1f85173ca3e3b8fb35749eae62735b193
Ksenia-cha <kseniyachapygova@gmail.com>
2023-02-27 08:11:51 +0300

    replace cam from rtsp to SConf struct

--- internal/rtsp-simple-server/interface.go
+++ internal/rtsp-simple-server/interface.go
@@ -3,7 +3,6 @@ package rtspsimpleserver
 import (
 	"context"
 
-	"github.com/Kseniya-cha/System-for-raising-video-streams/internal/refreshstream"
 	ce "github.com/Kseniya-cha/System-for-raising-video-streams/pkg/customError"
 )
 
@@ -11,11 +10,11 @@ type RTSPCommon interface {
 	// GetRtsp отправляет GET запрос на получение данных
 	GetRtsp(ctx context.Context, dataRTSPchan chan SConf) ([]SConf, ce.IError)
 	// PostAddRTSP отправляет POST запрос на добавление потока
-	PostAddRTSP(camDB refreshstream.RefreshStream) ce.IError
+	PostAddRTSP(camDB SConf) ce.IError
 	// PostRemoveRTSP отправляет POST запрос на удаление потока
-	PostRemoveRTSP(camRTSP string) ce.IError
+	PostRemoveRTSP(camRTSP SConf) ce.IError
 	// PostEditRTSP отправляет POST запрос на изменение потока
-	PostEditRTSP(camDB refreshstream.RefreshStream, conf SConf) ce.IError
+	PostEditRTSP(cam SConf) ce.IError
 }
 
 type RTSPUseCase interface {
