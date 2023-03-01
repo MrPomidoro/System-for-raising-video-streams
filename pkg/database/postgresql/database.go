834c743536977f66d8ae68c1aec735bb588d5586
Ksenia-cha <kseniyachapygova@gmail.com>
2023-03-01 11:55:44 +0300

    correct interrupt & reconnect

--- pkg/database/postgresql/database.go
+++ pkg/database/postgresql/database.go
@@ -26,17 +26,19 @@ func NewDB(ctx context.Context, cfg *config.Database, log *zap.Logger) (db *DB, 
 }
 
 func (db *DB) KeepAlive(ctx context.Context, log *zap.Logger, errCh chan error) {
-	defer close(errCh)
 
 	for {
 		if ctx.Err() != nil {
+			close(errCh)
 			return
 		}
+
 		go db.ping(ctx, errCh)
 
 		time.Sleep(3 * time.Second)
 		select {
 		case <-ctx.Done():
+			close(errCh)
 			return
 		case err := <-errCh:
 			log.Debug(fmt.Sprintf("cannot connect to database: %s", err))
@@ -62,16 +64,36 @@ func (db *DB) KeepAlive(ctx context.Context, log *zap.Logger, errCh chan error) 
 }
 
 func (db *DB) ping(ctx context.Context, errCh chan error) {
+	if ctx.Err() != nil {
+		return
+	}
+
 	conn, err := db.Conn.Acquire(context.Background())
 	if err != nil {
-		errCh <- fmt.Errorf("failed to acquire connection: %w", err)
+		select {
+		case <-ctx.Done():
+			return
+		case errCh <- fmt.Errorf("failed to acquire connection: %w", err):
+		}
 		return
 	}
+
+	tx, _ := conn.Begin(ctx)
 	defer conn.Release()
-	if _, err = conn.Exec(context.Background(), "SELECT 1"); err != nil {
-		errCh <- fmt.Errorf("failed to execute test query: %w", err)
+
+	if _, err = tx.Exec(context.Background(), "SELECT 1"); err != nil {
+		select {
+		case <-ctx.Done():
+			return
+		case errCh <- fmt.Errorf("failed to execute test query: %w", err):
+		}
 		return
 	}
+
+	// if _, err = conn.Query(context.Background(), "SELECT 1"); err != nil {
+	// 	errCh <- fmt.Errorf("failed to execute test query: %w", err)
+	// 	return
+	// }
 }
 
 // func (db *DB) ping(ctx context.Context, errChan chan error) {
