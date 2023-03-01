281812d21bf7e24cef97542e6fc298160ea1a562
Ksenia-cha <kseniyachapygova@gmail.com>
2023-03-01 12:21:17 +0300

    remove extra errors processing

--- cmd/model.go
+++ cmd/model.go
@@ -41,12 +41,12 @@ type app struct {
 
 // NewApp инициализирует прототип приложения
 func NewApp(ctx context.Context, cfg *config.Config) (*app, ce.IError) {
-	err := ce.ErrorApp
+	// err := ce.ErrorApp
 	log := logger.NewLogger(cfg)
 
 	db, e := postgresql.NewDB(ctx, &cfg.Database, log)
 	if e != nil {
-		return nil, err.SetError(e)
+		return nil, e
 	}
 
 	sigChan := make(chan os.Signal, 1)
@@ -63,7 +63,5 @@ func NewApp(ctx context.Context, cfg *config.Config) (*app, ce.IError) {
 		refreshStreamRepo: rsrepo.NewRepository(db, log),
 		statusStreamRepo:  ssrepo.NewRepository(db, log),
 		rtspRepo:          rtsprepo.NewRepository(cfg, log),
-
-		err: err,
 	}, nil
 }
