2c67e2ce53bd5702c9aaa7f337bafdd876b5adcb
Ksenia-cha <kseniyachapygova@gmail.com>
2023-03-01 07:36:20 +0300

    edit code of errors

--- pkg/customError/cErrors.go
+++ pkg/customError/cErrors.go
@@ -17,28 +17,28 @@ var (
 
 	ErrorApp = &Error{
 		level: FatalLevel,
-		code:  "50.2.1",
+		code:  "50.3.1",
 		desc:  "error at app operation level",
 		err:   nil,
 	}
 
 	ErrorRefreshStream = &Error{
 		level: ErrorLevel,
-		code:  "50.2.2",
+		code:  "50.1.2",
 		desc:  "refresh stream entity error at database operation level",
 		err:   nil,
 	}
 
 	ErrorStatusStream = &Error{
 		level: ErrorLevel,
-		code:  "50.2.3",
+		code:  "50.1.3",
 		desc:  "status stream entity error at database operation level",
 		err:   nil,
 	}
 
 	ErrorRTSP = &Error{
 		level: ErrorLevel,
-		code:  "50.3.1",
+		code:  "50.2.1",
 		desc:  "rtsp-simple-server entity error at database operation level",
 		err:   nil,
 	}
