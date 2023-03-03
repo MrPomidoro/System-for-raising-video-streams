9740ebef48f99d98f435e8867d4e296477121c86
Ksenia-cha <kseniyachapygova@gmail.com>
2023-03-03 15:35:32 +0300

    decommented methods for insert into status_stream table

--- cmd/methodsDB.go
+++ cmd/methodsDB.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	rtsp "github.com/Kseniya-cha/System-for-raising-video-streams/internal/rtsp-simple-server"
+	"github.com/Kseniya-cha/System-for-raising-video-streams/internal/statusstream"
 	ce "github.com/Kseniya-cha/System-for-raising-video-streams/pkg/customError"
 )
 
@@ -18,12 +19,12 @@ func (a *app) insertIntoStatusStream(method string, ctx context.Context, cam rts
 			return a.err.SetError(ctx.Err())
 		}
 
-		// insertStructStatusStream := statusstream.StatusStream{StreamId: cam.Id, StatusResponse: false}
-		// err = a.statusStreamRepo.Insert(ctx, &insertStructStatusStream)
-		// if err != nil {
-		// 	return a.err.SetError(fmt.Errorf("cannot insert stream %s to table status_stream", cam.Stream))
-		// }
-		// a.log.Debug("Success insert to table status_stream")
+		insertStructStatusStream := statusstream.StatusStream{StreamId: cam.Id, StatusResponse: false}
+		err = a.statusStreamRepo.Insert(ctx, &insertStructStatusStream)
+		if err != nil {
+			return a.err.SetError(fmt.Errorf("cannot insert stream %s to table status_stream", cam.Stream))
+		}
+		a.log.Debug("Success insert to table status_stream")
 
 		return nil
 	}
@@ -34,12 +35,12 @@ func (a *app) insertIntoStatusStream(method string, ctx context.Context, cam rts
 		return a.err.SetError(ctx.Err())
 	}
 
-	// insertStructStatusStream := statusstream.StatusStream{StreamId: cam.Id, StatusResponse: true}
-	// err = a.statusStreamRepo.Insert(ctx, &insertStructStatusStream)
-	// if err != nil {
-	// 	return a.err.SetError(fmt.Errorf("cannot insert to table status_stream"))
-	// }
-	// a.log.Debug("Success insert to table status_stream")
+	insertStructStatusStream := statusstream.StatusStream{StreamId: cam.Id, StatusResponse: true}
+	err = a.statusStreamRepo.Insert(ctx, &insertStructStatusStream)
+	if err != nil {
+		return a.err.SetError(fmt.Errorf("cannot insert to table status_stream"))
+	}
+	a.log.Debug("Success insert to table status_stream")
 
 	return nil
 }
