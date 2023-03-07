60372548463b8077fe2989d267b27b46facdb934
DmitrijSemenkin <dmitrijsemenkin@gmail.com>
2023-03-07 10:25:33 +0300

    add test connect database

--- pkg/database/postgresql/database_test.go
+++ pkg/database/postgresql/database_test.go
@@ -2,73 +2,36 @@ package postgresql
 
 import (
 	"context"
-	"testing"
-
-	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/config"
-	"github.com/Kseniya-cha/System-for-raising-video-streams/pkg/logger"
-	"github.com/stretchr/testify/assert"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
-// func TestNewDB(t *testing.T) {
-// 	cfg := config.Config{
-// 		Database: config.Database{
-// 			User:     "sysadmin",
-// 			Password: "w3X{77PpCR",
-// 			Host:     "192.168.0.32",
-// 			DbName:   "www",
-// 			Driver:   "postgres",
-// 			Port:     5432,
-// 		},
-// 	}
-// 	log := logger.NewLogger(&cfg)
-// 	ctx := context.Background()
-// 	pool, err := pgxpool.NewWithConfig(ctx, getConfig(cfg.Database))
-// 	if err != nil {
-// 		t.Error("unexpected error:", err)
-// 	}
-// 	defer pool.Close()
+// В этом тесте можно обойтись и без моков, так как мы проверяем фактическое подключение к бд
+func TestDatabaseConnection(t *testing.T) {
+	// задаем параметры подключения к базе данных
+	config, _ := pgxpool.ParseConfig("")
+	config.ConnConfig.User = "sysadmin"
+	config.ConnConfig.Password = "w3X{77PpCR"
+	config.ConnConfig.Host = "192.168.0.32"
+	config.ConnConfig.Port = 5432
+	config.ConnConfig.Database = "www"
 
-// 	poolNew, err := NewDB(ctx, cfg.Database, log)
-// 	if err != nil {
-// 		t.Error("unexpected error:", err)
-// 	}
-
-// 	assert.NotNil(t, poolNew)
-// 	assert.Equal(t, poolNew.Conn, pool)
-// }
-
-func TestNewDB(t *testing.T) {
-	cfg := config.Config{
-		Database: config.Database{User: "sysadmin",
-			Password: "w3X{77PpCR",
-			Host:     "192.168.0.32",
-			DbName:   "www",
-			Driver:   "postgres",
-			Port:     5432,
-		},
-	} // задаем конфигурацию для базы данных
-	log := logger.NewLogger(&cfg)
-	// создаем mock объект для pgxpool.Pool
-	db, mock, err := sqlmock.New()
+	// создаем пул соединений к базе данных
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 	require.NoError(t, err)
-	defer db.Close()
+	defer pool.Close()
 
-	// pool, err := pgxpool.NewWithConfig(context.Background(), getConfig(cfg.Database))
-	// pool, err := pgxpool.Connect(context.Background(), "postgres://user:password@localhost/dbname")
-	// require.NoError(t, err)
+	// делаем запрос к базе данных
+	conn, err := pool.Acquire(context.Background())
+	if err != nil {
+		t.Fatalf("error acquiring connection from pool: %s", err)
+	}
+	defer conn.Release()
 
-	// создаем объект для тестирования
-	// testDB := &DB{pool}
+	_, err = conn.Exec(context.Background(), "SELECT 1")
+	if err != nil {
+		t.Fatalf("error executing query: %s", err)
+	}
 
-	// имитируем вызов pgxpool.NewWithConfig()
-	mock.ExpectQuery("SELECT 1").WillReturnRows(sqlmock.NewRows([]string{"1"}).AddRow("1"))
-	// newPool, err := testDB.NewPool(ctx, cfg)
-	newPool, err := NewDB(context.Background(), cfg.Database, log)
-	assert.NoError(t, err)
-	assert.NotNil(t, newPool)
-
-	// проверяем, что все вызовы mock объекта были выполнены
-	assert.NoError(t, mock.ExpectationsWereMet())
 }
