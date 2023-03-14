8680b2474296316dd429f16ce569a3c4d1c500c4
Ksenia-cha <kseniyachapygova@gmail.com>
2023-03-14 11:56:01 +0300

    change query to db

--- internal/refreshstream/constants.go
+++ internal/refreshstream/constants.go
@@ -2,12 +2,12 @@ package refreshstream
 
 const (
 	QueryStateTrue = `SELECT "id", "login", "pass", "ip_address_out", "cam_path",
-	"code_mp", "state_public", "status_public", "protocol"
+	"code_mp", "state_public", "status_public"
 	FROM public."%s"
 	WHERE "cam_path" IS NOT null AND "state_public" = 1`
 
 	QueryStateFalse = `SELECT "id", "login", "pass", "ip_address_out", "cam_path",
-	"code_mp", "state_public", "status_public", "protocol"
+	"code_mp", "state_public", "status_public"
 	FROM public."%s"
 	WHERE "cam_path" IS NOT null AND "state_public" = 0`
 )
